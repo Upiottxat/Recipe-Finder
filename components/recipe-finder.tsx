dfa33b047ca11ef826a4f6e41ef0dcb099de55e7
Saini <85294275+Upiottxat@users.noreply.github.com>
2025-08-30 13:01:00 +0530

    upadate

--- components/recipe-finder.tsx
+++ components/recipe-finder.tsx
@@ -46,7 +46,7 @@ export default function RecipeFinder() {
       <div className="flex flex-col gap-4">
         <IngredientInput value={ingredients} onChange={setIngredients} />
         <div className="flex items-center gap-2">
-          <FilterBar value={filters} onChange={setFilters} />
+          {/* <FilterBar value={filters} onChange={setFilters} /> */}
           <Button variant="secondary" onClick={handleClear} className="ml-auto">
             Clear ingredients
           </Button>
